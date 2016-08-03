e1446682b23116e15e205977e1088f88c2993fa2
Brian Bland <bbland@airware.com>
2016-08-02 17:37:20 -0700

    Show correct "deployed at" time for deployments
    Previously this showed the creation timestamp for the deployment, which
    only indicates when the app deployment was first created. Now this
    instead shows the creation timestamp of the most recent replica set.

--- kube/replicasets.go
+++ kube/replicasets.go
@@ -3,7 +3,9 @@ package kube
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/url"
+	"strings"
 
 	"github.com/airware/vili/kube/extensions/v1beta1"
 	"github.com/airware/vili/kube/unversioned"
@@ -32,6 +34,17 @@ func (s *ReplicaSetsService) List(env string, query *url.Values) (*v1beta1.Repli
 		return nil, status, err
 	}
 	return resp, nil, nil
+}
+
+// ListForDeployment fetches the list of replicasets in `env` for the given deployment
+func (s *ReplicaSetsService) ListForDeployment(env string, deployment *v1beta1.Deployment) (*v1beta1.ReplicaSetList, *unversioned.Status, error) {
+	var selector []string
+	for k, v := range deployment.Spec.Selector.MatchLabels {
+		selector = append(selector, fmt.Sprintf("%s=%s", k, v))
+	}
+	query := &url.Values{}
+	query.Add("labelSelector", strings.Join(selector, ","))
+	return s.List(env, query)
 }
 
 // Get fetches the replicaset in `env` with `name`
