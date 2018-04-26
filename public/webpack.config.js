f7253bc8eea5f1cff7c5eda377ad52f5bbc2e9af
Volkan <vgurel@airware.com>
2018-04-26 06:50:28 -0700

    Update webpack build code to work with updated webpack version

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,49 +1,56 @@
-'use strict'
+"use strict"
 
-const path = require('path')
-const webpack = require('webpack')
-const autoprefixer = require('autoprefixer')
+const path = require("path")
+const webpack = require("webpack")
+const FailPlugin = require("webpack-fail-plugin")
 
-const CleanPlugin = require('./webpack/plugins/CleanPlugin')
-const GzipPlugin = require('./webpack/plugins/GzipPlugin')
-const RevPlugin = require('./webpack/plugins/RevPlugin')
-const Loaders = require('./webpack/Loaders')
+const CleanPlugin = require("./webpack/plugins/CleanPlugin")
+const GzipPlugin = require("./webpack/plugins/GzipPlugin")
+const RevPlugin = require("./webpack/plugins/RevPlugin")
+const Loaders = require("./webpack/Loaders")
 
 // A webpack config abstraction
 class WebpackConfig {
-
-  constructor () {
+  constructor() {
     this.absoluteRoot = __dirname
 
     this.IS_DEVELOP = !process.env.WEBPACK_PROD
 
-    this.shouldOptimize = process.env.WEBPACK_PROD && ['develop', 'master'].indexOf(process.env.BRANCH_NAME) >= 0
+    this.shouldOptimize =
+      process.env.WEBPACK_PROD &&
+      ["develop", "master"].indexOf(process.env.BRANCH_NAME) >= 0
     if (this.shouldOptimize) {
-      console.log(`Running optimizations due to branch: ${process.env.BRANCH_NAME}`); // eslint-disable-line
+      console.log(
+        `Running optimizations due to branch: ${process.env.BRANCH_NAME}`
+      ) // eslint-disable-line
     }
   }
 
-  get resolve () {
+  get mode() {
+    return this.shouldOptimize ? "production" : "development"
+  }
+
+  get resolve() {
     return {
-      extensions: ['', '.js', '.json']
+      extensions: [".js", ".json"],
     }
   }
 
-  get entry () {
+  get entry() {
     return {
-      app: path.join(this.absoluteRoot, 'src', 'app')
+      app: path.join(this.absoluteRoot, "src", "app"),
     }
   }
 
-  get output () {
+  get output() {
     return {
-      filename: 'app-[hash]' + (this.IS_DEVELOP ? '' : '.min') + '.js',
-      path: path.join(this.absoluteRoot, 'build'),
-      publicPath: './'
+      filename: "app-[hash]" + (this.IS_DEVELOP ? "" : ".min") + ".js",
+      path: path.join(this.absoluteRoot, "build"),
+      publicPath: "./",
     }
   }
 
-  get plugins () {
+  get plugins() {
     let plugins = []
     const pluginList = this.pluginList
     for (var i = 0; i < pluginList.length; i++) {
@@ -57,50 +64,40 @@ class WebpackConfig {
     return plugins
   }
 
-  get pluginList () {
+  get pluginList() {
     return [
       this.cleanPlugin,
-      this.dedupePlugin,
+      this.failPlugin,
       this.revPlugin,
-      this.definePlugin
+      this.definePlugin,
     ]
   }
 
-  get prodPlugins () {
-    const UGLIFY_OPTIONS = {
-      compress: {
-        warnings: false
-      }
-    }
-
-    return [
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.optimize.UglifyJsPlugin(UGLIFY_OPTIONS),
-      new GzipPlugin()
-    ]
+  get prodPlugins() {
+    return [new GzipPlugin()]
   }
 
-  get cleanPlugin () {
-    return new CleanPlugin(path.join(this.absoluteRoot, 'build'))
+  get cleanPlugin() {
+    return new CleanPlugin(path.join(this.absoluteRoot, "build"))
   }
 
-  get dedupePlugin () {
-    return new webpack.optimize.DedupePlugin()
+  get failPlugin() {
+    return FailPlugin
   }
 
-  get revPlugin () {
+  get revPlugin() {
     return new RevPlugin({
-      path: path.join(this.absoluteRoot, './build/stats.json'),
-      notifyTitle: this.app
+      path: path.join(this.absoluteRoot, "./build/stats.json"),
+      notifyTitle: this.app,
     })
   }
 
-  get definePlugin () {
+  get definePlugin() {
     if (this.shouldOptimize) {
       const DEFINE_PROD_ENV = {
-        'process.env': {
-          'NODE_ENV': '"production"'
-        }
+        "process.env": {
+          NODE_ENV: '"production"',
+        },
       }
 
       return new webpack.DefinePlugin(DEFINE_PROD_ENV)
@@ -109,38 +106,38 @@ class WebpackConfig {
       // inside your code for any environment checks; UglifyJS will automatically
       // drop any unreachable code.
       return new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-        }
+        "process.env": {
+          NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        },
       })
     }
   }
 
-  get devtool () {
-    return this.IS_DEVELOP ? 'eval' : 'source-map'
+  get devtool() {
+    return this.IS_DEVELOP ? "eval" : "source-map"
   }
 
-  get node () {
+  get node() {
     // Some node_modules pull in Node-specific dependencies.
     // Since we're running in a browser we have to stub them out. See:
     // https://webpack.github.io/docs/configuration.html#node
     // https://github.com/webpack/node-libs-browser/tree/master/mock
     // https://github.com/webpack/jade-loader/issues/8#issuecomment-55568520
     return {
-      fs: 'empty',
-      child_process: 'empty',
-      net: 'empty',
-      tls: 'empty'
+      fs: "empty",
+      child_process: "empty",
+      net: "empty",
+      tls: "empty",
     }
   }
 
-  get module () {
+  get module() {
     return {
-      loaders: this.loaders
+      rules: this.loaders,
     }
   }
 
-  get loaders () {
+  get loaders() {
     let loaders = []
     const loaderList = this.loaderList
     for (var i = 0; i < loaderList.length; i++) {
@@ -151,52 +148,41 @@ class WebpackConfig {
     return loaders
   }
 
-  get loaderList () {
+  get loaderList() {
     return [
       this.javascriptLoader,
       this.lessLoader,
       this.jsonLoader,
-      this.assetLoader
+      this.assetLoader,
     ]
   }
 
-  get javascriptLoader () {
+  get javascriptLoader() {
     return new Loaders.JavascriptLoader()
   }
 
-  get lessLoader () {
+  get lessLoader() {
     return new Loaders.LessLoader()
   }
 
-  get jsonLoader () {
+  get jsonLoader() {
     return new Loaders.JSONLoader()
   }
 
-  get assetLoader () {
+  get assetLoader() {
     return new Loaders.AssetLoader()
   }
 
-  get postcss () {
-    const PREFIX_BROWSERS = [
-      'Chrome >= 20',
-      'Firefox >= 24',
-      'Explorer >= 9',
-      'Safari >= 6'
-    ]
-
-    return [autoprefixer(PREFIX_BROWSERS)]
-  }
-
-  toJSON () {
+  toJSON() {
     let json = {
+      mode: this.mode,
       entry: this.entry,
       output: this.output,
       resolve: this.resolve,
-      devtool: this.devtool,
       plugins: this.plugins,
+      devtool: this.devtool,
       node: this.node,
       module: this.module,
-      postcss: this.postcss
     }
 
     return json
@@ -205,6 +191,3 @@ class WebpackConfig {
 
 const config = new WebpackConfig()
 module.exports = config.toJSON()
-
-// export for tests to to work
-module.exports.WebpackConfig = WebpackConfig
