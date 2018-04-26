b783bdcb8e2194c19d68de3276b5a5a301e0f6d1
Volkan <vgurel@airware.com>
2018-04-25 21:40:26 -0700

    Update npm dependency versions

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -1,23 +1,21 @@
-import { reducer as uiReducer } from 'redux-ui'
-import { routerReducer } from 'react-router-redux'
+import { routerReducer } from "react-router-redux"
 
-import app from './app'
-import envs from './envs'
-import releases from './releases'
-import deployments from './deployments'
-import replicaSets from './replicaSets'
-import jobs from './jobs'
-import jobRuns from './jobRuns'
-import configmaps from './configmaps'
-import pods from './pods'
-import nodes from './nodes'
+import app from "./app"
+import envs from "./envs"
+import releases from "./releases"
+import deployments from "./deployments"
+import replicaSets from "./replicaSets"
+import jobs from "./jobs"
+import jobRuns from "./jobRuns"
+import configmaps from "./configmaps"
+import pods from "./pods"
+import nodes from "./nodes"
 
-function hardcodedValueReducer (state = null, action) {
+function hardcodedValueReducer(state = null, action) {
   return state
 }
 
 const reducers = {
-  ui: uiReducer,
   routing: routerReducer,
   user: hardcodedValueReducer,
   defaultEnv: hardcodedValueReducer,
@@ -30,7 +28,7 @@ const reducers = {
   jobRuns,
   configmaps,
   pods,
-  nodes
+  nodes,
 }
 
 export default reducers
